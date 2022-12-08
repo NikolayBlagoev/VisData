15b36618e7140c87a1db4d3cbaaf6d408c3f5d5f
William Narchi <willy.narchi@gmail.com>
2022-12-08 14:30:36 +0100

    Update SteamSpy CCU gathering script; Add data for 2022-12-08

--- datasets/fetch_apis_steamspy.py
+++ datasets/fetch_apis_steamspy.py
@@ -1,22 +1,33 @@
 import json
 import requests
 from datetime import date
+from os import getcwd, mkdir, path
 from tqdm import trange
 
-BATCHES = 2 # Number of batches to process; Final number of requests is (BATCHES * PROCESS_COUNT); Each batch results in one JSON file
+BATCHES = 56 # Number of batches to process; Final number of requests is (BATCHES * PROCESS_COUNT); Each batch results in one JSON file
 COLD_START = 0 # First index to start requests from
 PROCESS_COUNT = 1000 # How many games to make requests for per batch
 
+CWD = getcwd()
+TARGET_DIR = "steamspy"
+TODAY_STR = str(date.today())
+
 if __name__ == "__main__":
+    # Create destination directory if it does not exist
+    dst_dir = path.join(CWD, TARGET_DIR, TODAY_STR)
+    if not path.exists(dst_dir):
+        mkdir(dst_dir)
+
     with open("data_set.json") as fl:
         processed_data = json.load(fl)
 
     for current_batch in trange(BATCHES):
         start = COLD_START + (current_batch * PROCESS_COUNT)
+        end = min(start + PROCESS_COUNT, len(processed_data))
         data = []
 
-        for game_idx in trange(start, start + PROCESS_COUNT,
-                               desc=f"Fetching data for range [{start}-{start + (PROCESS_COUNT - 1)}]"):
+        for game_idx in trange(start, end,
+                               desc=f"Fetching data for range [{start}-{end - 1}]"):
             game = processed_data[game_idx]
             app_id = game["appid"]
 
@@ -27,6 +38,6 @@ if __name__ == "__main__":
             entry["retrieval_date"] = str(date.today())
             data.append(entry)
 
-        with open(f"steamspy/tmp_{start}-{start + (PROCESS_COUNT - 1)}.json", "w") as output_file:
+        with open(f"{TARGET_DIR}/{TODAY_STR}/tmp_{start}-{end - 1}.json", "w") as output_file:
             json.dump(data, output_file, indent=2)  
             
