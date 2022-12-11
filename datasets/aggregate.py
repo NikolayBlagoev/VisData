129dc1cd59ee371d000324d723830685c50ceb6f
NikolayBlagoev <nickyblagoev@gmail.com>
2022-12-11 02:29:48 +0200

    CringeDB wins

--- datasets/aggregate.py
+++ datasets/aggregate.py
@@ -52,6 +52,56 @@ def get_std_median_like_count_per_genre(raw):
     with open("likes_count_genres.json","w") as f:
         json.dump(new_dt,f, indent=2)
 
+def get_count_price(raw):
+    dt = {"free": 0, "0-99": 0,"100-499": 0 ,"500-999": 0,  "1000-1999": 0, "2000-2999": 0,"3000-4999": 0, "5000-5999": 0, "6000-9999": 0, "10000+": 0}
+    for game in raw:
+        
+        price = int(game["price"])
+        if price == 0:
+            dt["free"] += 1
+        elif price < 100:
+            dt["0-99"] += 1
+        elif price < 500:
+            dt["100-499"] += 1
+        elif price<1000:
+            dt["500-999"] += 1
+        elif price < 2000:
+            dt["1000-1999"] += 1
+        elif price < 3000:
+            dt["2000-2999"] += 1
+        elif price < 5000:
+            dt["3000-4999"] += 1
+        elif price < 6000:
+            dt["5000-5999"] += 1
+        elif price < 10000:
+            dt["6000-9999"] += 1
+        else:
+            dt["10000+"] += 1
+   
+    with open("price_counts.json","w") as f:
+        json.dump(dt,f, indent=2)
+
+def get_std_median_likes_total(raw):
+    dt = {"root": []}
+    for game in raw:
+        try:
+            rating = game["positive"]/(game["positive"]+game["negative"])
+        except ZeroDivisionError:
+            rating = 0
+        dt["root"].append(rating)
+    # print(dt.keys())
+    new_dt = dict()
+    for k,v in dt.items():
+        arr = np.array(v)
+        arr.sort()
+        new_dt[k]={"mean": arr.mean(), "std": arr.std(), "max": arr.max(), "min": arr.min(), "median": np.median(arr),
+        "10th": np.percentile(arr,10),
+        "20th": np.percentile(arr,20), "25th": np.percentile(arr,25), "30th": np.percentile(arr,30), 
+        "40th": np.percentile(arr,40), "50th": np.percentile(arr,50),
+        "60th": np.percentile(arr,60), "70th": np.percentile(arr,70), "75th": np.percentile(arr,75),
+        "80th": np.percentile(arr,80), "90th": np.percentile(arr,90), "99th": np.percentile(arr,99)}
+    with open("total_likes.json","w") as f:
+        json.dump(new_dt,f, indent=2)
 with open("data_set.json") as fl:
     raw = json.load(fl)
-    get_std_median_like_count_per_genre(raw)
+    get_std_median_likes_total(raw)
--- datasets/net_scrape_cringedb.py
+++ datasets/net_scrape_cringedb.py
@@ -1,22 +1,48 @@
 from bs4 import BeautifulSoup
 from tqdm import trange
 import urllib.request
+import requests
 import json
 import time
 import re  
 import json
 import time
+import random 
+from fp.fp import FreeProxy
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+prox = FreeProxy(https=True).get()
+prox = "24"
+print(prox[:1])
+exit()
+fp=Options()
+fp.set_preference('network.proxy.type', 1)
+fp.set_preference('network.proxy.ssl',"154.85.55.174" )
+fp.set_preference('network.proxy.ssl_port', 3128)
 
+driver = webdriver.Firefox(options=fp)
+driver.set_page_load_timeout(10)
+driver.get("https://whatismyipaddress.com/")
+
+print(driver.page_source)
+exit()
+class PROXY():
+    def __init__(self, ip, cookie) -> None:
+        self.ip = ip
+        self.cookie = cookie
+        pass
 BASE_URL = "http://steamdb.info/app/"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0',
 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-'Cookie': 'cf_clearance=4ytoWH01u2FLBl8nDF1PSrEdEaKsaPPi8_Yg75H5Pmc-1670685728-0-160; cf_chl_2=21319eda1002ea0; __cf_bm=VRS5SwqkQis7fjDj95UJ4tHgvbvlmVE7nJckqlJZuLc-1670685728-0-AayMWjrSOicvUVf4z+30mWcwmMZRDvS8dONjsO0XCAVKGmXxj3bNDbLpONvG+9VparIrW8mi6sVtiXJgzz8cn6A=',
+'Cookie': 'cf_clearance=WvRndt7ArTUyJguFR_ayykvGFEayOM2Fepyyxe6jxyQ-1670715909-0-250; cf_chl_2=cd20d71b0446097; __cf_bm=aZu9tJhE42Zh_uMPkvUpDaXVOP3HPfzW7fxRIDUt6oI-1670716691-0-Afzi5cm0hAZLn/YaNW9Eg8MDAMgfj4daY6OLvq8wBT0RF2n2R8crc08WfiQjyZMNYkDKP9Vj7I86QW0dBJxldAQ=',
 'Upgrade-Insecure-Requests': '1',
 'Connection':'keep-alive'}
 BATCHES = 28 # Number of batches to process; Final number of requests is (BATCHES * PROCESS_COUNT); Each batch results in one JSON file
 COLD_START = 0 # First index to start requests from
 PROCESS_COUNT = 1000 # How many games to make requests for per batch
-
+PROXIES = None
 def create_url(inp: str) -> str :
     inp = inp.strip()
     inp = re.sub(" ","-",inp)
@@ -26,9 +52,15 @@ def create_url(inp: str) -> str :
 def get_game_info(appid, data_list):
     steamchart = BASE_URL + str(appid)+"/graphs/"
     page = urllib.request.Request(steamchart, headers=HEADERS)
+    if PROXIES is not None:
+        page.set_proxy(PROXIES,'https')
+        page.set_proxy(PROXIES,'http')
     content = urllib.request.urlopen(page).read()
+    # response = requests.get(steamchart, headers=HEADERS).text
+    # print(response.content)
+    
     soup = BeautifulSoup(content, 'html.parser')    
-                   
+    
     try:
         data = soup.findAll("li")
         peak = -1
@@ -43,8 +75,12 @@ def get_game_info(appid, data_list):
         'appid' : appid,
         'peak_count' : peak
     }
+    print()
+    print()
+    print(entry)
+    
     data_list.append(entry)
-    time.sleep(0.005)
+    time.sleep(0.1)
     return data_list
 def make_attempt(appid,data_list):
     try:
@@ -54,7 +90,9 @@ def make_attempt(appid,data_list):
         print()
         print(f'ERROR {BASE_URL+str(appid)}')
         print(e)
-        time.sleep(30)
+        # time.sleep(10)
+        PROXIES = FreeProxy(https=True).get()
+        print(PROXIES)
         return make_attempt(appid,data_list)
     
 if __name__ == "__main__":
